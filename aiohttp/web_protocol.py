ab12973634d75f7d6d2165fa58f33bdbe9a6ad54
Nikolay Bryskin <nb@gosu.ai>
2020-10-13 12:37:36 +0300

    Add exc_info to 504 error log

--- aiohttp/web_protocol.py
+++ aiohttp/web_protocol.py
@@ -423,7 +423,7 @@ class RequestHandler(BaseProtocol):
                     break
                 except asyncio.TimeoutError as exc:
                     self.log_debug('Request handler timed out.', exc_info=exc)
-                    resp = self.handle_error(request, 504)
+                    resp = self.handle_error(request, 504, exc)
                 except Exception as exc:
                     resp = self.handle_error(request, 500, exc)
                 else:
